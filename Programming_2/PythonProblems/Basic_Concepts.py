2d883c1d5acabfb0fda55193ca67dca5e5be8cfb
19kzheng598 <kevz3680@gmail.com>
2023-03-26 17:45:35 -0400

    commit

---
+++ Programming_2/PythonProblems/Basic_Concepts.py
@@ -0,0 +1,50 @@
+"""
+Your First Program
+
+
+Write a program to print "Python is fun".
+Note that the sentence starts with a capital letter.
+
+Hint
+Use print() function."""
+# output "Python is fun"
+print("Python is fun")
+
+"""Simple Operations
+
+
+Today is a holiday at the children's camp, so all the children will be served ice cream.
+There are 68 children in the group, and each child should get 2 scoops of ice cream.
+
+Task
+Write a program to calculate and output the total number of ice cream scoops we need."""
+# your code goes here
+print(68 * 2)
+
+
+"""Quotient
+
+
+Calculate and output the number of miles in 1000 kilometers.
+
+Hint
+One mile is 1.6 kilometers, so find the quotient of 1000 and 1.6."""
+# your code goes here
+print(1000//1.6)
+
+
+"""
+Exponentiation is the raising of one number to the power of another.
+This operation is performed using two asterisks **.
+
+Let's use exponentiation to solve a known problem.
+You are offered a choice of either $1.000.000 or $0.01 (one penny) doubled every day for 30 days (the resulting amount is doubled every day).
+Task:
+Write a program to calculate the amount that will result from the doubling to understand which choice results in a larger amount.
+
+Hint:
+Let's see how exponentiation can be useful to perform the calculation.
+For example, if we want to calculate how much money we will have on the 5th day, we can use this expression: 0.01*(2**5) = 0.32 dollars (multiply the penny by 2 raised to the power of 5)."""
+# your code goes here
+print(.01*(2**30))
+###
