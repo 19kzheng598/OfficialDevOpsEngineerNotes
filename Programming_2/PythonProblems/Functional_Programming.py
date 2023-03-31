ceecec9780216da614fd7fa79352cfcd85e2963f
19kzheng598 <kevz3680@gmail.com>
2023-03-30 23:15:46 -0400

    update

--- Programming_2/PythonProblems/Functional_Programming.py
+++ Programming_2/PythonProblems/Functional_Programming.py
@@ -44,3 +44,65 @@ def words():
 
 
 print(list(words()))
+
+
+"""Decorators
+
+
+You are given code that takes input and prints it as a simple row of text.
+
+Add the uppercase_decorator to make the text uppercase."""
+text = input()
+
+
+def uppercase_decorator(func):
+    def wrapper(text):
+        # your code goes here
+        return (text.upper())
+    return wrapper
+
+
+@uppercase_decorator
+def display_text(text):
+    return (text)
+
+
+print(display_text(text))
+
+
+"""Recursion
+
+
+The provided code uses recursion to calculate the sum of all items in the input list.
+
+Change the code to calculate and output the sum of the squares of all the list items."""
+
+
+def calc(list):
+    if len(list) == 0:
+        return 0
+    else:
+        return list[0]**2 + calc(list[1:])
+
+
+list = [1, 3, 4, 2, 5]
+x = calc(list)
+print(x)
+
+
+"""Sets
+
+
+Sets are created using curly braces and they hold unique values.
+
+Given two sets, find and output all the elements that are common to both sets.
+For example, for the following sets:
+{'a', 'b', 'c'}
+{'c', 'd', 'e'}
+
+The output should be {'c'}, as it is present in both sets."""
+set1 = {2, 4, 5, 6}
+set2 = {4, 6, 7, 8, 11, 42, 2}
+
+# your code goes here
+print(set1 & set2)
--- Programming_2/PythonProblems/More_Types.py
+++ Programming_2/PythonProblems/More_Types.py
@@ -156,3 +156,26 @@ for new_word in txt_list:
         word = new_word
 
 print(word)
+
+
+# Useful functions
+print(", ".join(["spam", "eggs", "ham"]))
+# prints "spam, eggs, ham"
+
+print("Hello ME".replace("ME", "world"))
+# prints "Hello world"
+
+print("This is a sentence.".startswith("This"))
+# prints "True"
+
+print("This is a sentence.".endswith("sentence."))
+# prints "True"
+
+print("This is a sentence.".upper())
+# prints "THIS IS A SENTENCE."
+
+print("AN ALL CAPS SENTENCE".lower())
+# prints "an all caps sentence"
+
+print("spam, eggs, ham".split(", "))
+# prints "['spam', 'eggs', 'ham']"
